a53ceb8bc13963c6760a36eeb65c629a227400c5
WinsomeJoo <tmdwn0603@naver.com>
2023-07-20 18:11:31 +0900

    feat: Cody, Friend table에 @Builder 추가 #/7

--- todis-web/todis-web/src/main/java/com/todis/todisweb/demo/domain/Cody.java
+++ todis-web/todis-web/src/main/java/com/todis/todisweb/demo/domain/Cody.java
@@ -1,6 +1,7 @@
 package com.todis.todisweb.demo.domain;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -34,4 +35,15 @@ public class Cody {
     @Column(name="date")
     @CreationTimestamp
     private Timestamp date;
+
+    @Builder
+    public Cody(int id, int userId, int clothId, String comment, int likes, String image, Timestamp date) {
+        this.id = id;
+        this.userId = userId;
+        this.clothId = clothId;
+        this.comment = comment;
+        this.likes = likes;
+        this.image = image;
+        this.date = date;
+    }
 }
