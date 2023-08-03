36e4a5d7797b214a2a90fbb3a80748c09c4a5148
minjumost <minjumost@naver.com>
2023-08-03 20:35:46 +0900

    Update WeatherController.java

--- todis-web/todis-web/src/main/java/com/todis/todisweb/demo/controller/WeatherController.java
+++ todis-web/todis-web/src/main/java/com/todis/todisweb/demo/controller/WeatherController.java
@@ -8,19 +8,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.ProtocolException;
-import java.net.URL;
-import java.net.URLEncoder;
+import java.net.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-import com.todis.todisweb.demo.domain.OAuthToken;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -28,49 +29,61 @@ import org.springframework.web.client.RestTemplate;
 public class WeatherController{
 
     @GetMapping("/get")
-    public ResponseEntity<String> getWeatherInfo() throws UnsupportedEncodingException {
+    public String getWeatherInfo()
+            throws IOException, URISyntaxException {
+
+        //프론트에서 받아야하는 값 면 or 읍 or 동 주소
+
+        LocalDateTime now = LocalDateTime.now();
+        String yyyyMMdd = now.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        int hour = now.getHour();
+        int min = now.getMinute();
+        if(min <= 30) { // 해당 시각 발표 전에는 자료가 없음 - 이전시각을 기준으로 해야함
+            hour -= 1;
+        }
+        String hourStr = hour + "00"; // 정시 기준
+        System.out.println("여기입니다!!!!!!!!!!!");
+        System.out.println(hourStr);
+        String currentChangeTime = now.format(DateTimeFormatter.ofPattern("yy.MM.dd ")) + hour;
+
+
+        String apiUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst";
 
         String serviceKey = "CMdmnqOO8IjX29Thhm9UJ1iEuCCSaqqQHrZJB5VZZRd4HCCIVJ%2BJoPhs8JjctSaRCpKr2GWaemdig4ySJBWziw%3D%3D";
-        String encodedServiceKey = URLEncoder.encode(serviceKey, "UTF-8");
+        String dataType = "JSON";	//데이터 타입
+        String numOfRows = "11";	//한 페이지 결과 수
+        String pageNo = "12";	//페이지 번호
+        String base_date = "20230803";	//조회하고싶은 날짜
+        String base_time = "1400";	//조회하고 싶은 날짜의 시간 날짜
+        String nx = "55";
+        String ny = "127";
+
+        StringBuilder urlBuilder = new StringBuilder(apiUrl);
+        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "="+serviceKey);
+        urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode(dataType, "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode(numOfRows, "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode(pageNo, "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("base_date","UTF-8") + "=" + URLEncoder.encode(base_date, "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("base_time","UTF-8") + "=" + URLEncoder.encode(base_time, "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("nx","UTF-8") + "=" + URLEncoder.encode(nx, "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("ny","UTF-8") + "=" + URLEncoder.encode(ny, "UTF-8"));
+
+        /*
+         * GET방식으로 전송해서 파라미터 받아오기
+         */
+        URL url = new URL(urlBuilder.toString());
 
         RestTemplate rt = new RestTemplate();
 
-
-        // 오브젝트 생성
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("serviceKey", encodedServiceKey);
-        params.add("dataType", "JSON");
-        params.add("numOfRows", "10");
-        params.add("base_date", "20230803");
-        params.add("base_time", "1500");
-        params.add("nx", "60");
-        params.add("ny", "127");
-
-
-        //헤더와 바디 합쳐서 오브젝트 생성
-        HttpEntity<MultiValueMap<String, String>> WeatherInfoRequest =
-                new HttpEntity<>(params);
-
-        //요청 보내기
         ResponseEntity<String> response = rt.exchange(
-                "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst",
+                url.toURI(),
                 HttpMethod.GET,
-                WeatherInfoRequest,
+                null,
                 String.class  //응답받을 타입
         );
-    /*
-        ObjectMapper objectMapper = new ObjectMapper();
-        OAuthToken oAuthToken = null;
-        try {
-            oAuthToken = objectMapper.readValue(response.getBody(), OAuthToken.class);
-        }catch (JsonMappingException e) {
-            e.printStackTrace();
-        }catch (JsonProcessingException e){
-            e.printStackTrace();
-        }
-        */
-        return response;
 
-}
+
+        return response.toString();
+    }
 
 }
