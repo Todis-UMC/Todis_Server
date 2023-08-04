de2fe9fbdfc402c91929d0fcda2713187277b1ec
minjumost <minjumost@naver.com>
2023-08-04 12:04:03 +0900

    [fix] syntax error

--- todis-web/todis-web/src/main/java/com/todis/todisweb/demo/controller/FriendRequestController.java
+++ todis-web/todis-web/src/main/java/com/todis/todisweb/demo/controller/FriendRequestController.java
@@ -7,11 +7,7 @@ import com.todis.todisweb.demo.service.FriendRequestServiceImpl;
 import com.todis.todisweb.global.response.ResponseForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/friend")
