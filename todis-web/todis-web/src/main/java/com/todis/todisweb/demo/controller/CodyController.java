4e574120b3197bf1606c37ed75a841ba595ffa4d
hyeonji0730 <leehyeonji0730@naver.com>
2023-08-05 18:26:06 +0900

    코디 이미지 1개 업로드 구현

--- todis-web/todis-web/src/main/java/com/todis/todisweb/demo/controller/CodyController.java
+++ todis-web/todis-web/src/main/java/com/todis/todisweb/demo/controller/CodyController.java
@@ -7,10 +7,15 @@ import com.todis.todisweb.demo.dto.CodyDto;
 import com.todis.todisweb.demo.service.CodyServiceImpl;
 import com.todis.todisweb.global.response.ResponseForm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class CodyController {
@@ -22,6 +27,15 @@ public class CodyController {
     public ResponseForm postComment(Authentication authentication, String Comment) {
         codyService.updateComment(authentication.getName(),Comment);
         return ResponseForm.success(POST_SUCCESS.getCode(), POST_SUCCESS.getMessage(), null);
+    }
+
+    @PostMapping(path = "/cody/image", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseForm postCody(
+            Authentication authentication,
+            @RequestPart(value = "file", required = false) MultipartFile file
+    ){
+        String url = codyService.updateCody(authentication.getName(), file);
+        return ResponseForm.success(POST_SUCCESS.getCode(), POST_SUCCESS.getMessage(), url);
     }
 
 }
--- todis-web/todis-web/src/main/java/com/todis/todisweb/demo/service/CodyService.java
+++ todis-web/todis-web/src/main/java/com/todis/todisweb/demo/service/CodyService.java
@@ -1,8 +1,10 @@
 package com.todis.todisweb.demo.service;
 
 import com.todis.todisweb.demo.dto.CodyDto;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface CodyService {
 
     String updateComment(String email, String comment);
+    String updateCody(String email, MultipartFile file);
 }
