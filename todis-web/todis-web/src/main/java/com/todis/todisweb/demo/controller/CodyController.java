422a0951e4ee299487d94f948c8ab9c11e764ba9
hyeonji0730 <leehyeonji0730@naver.com>
2023-08-11 00:49:15 +0900

    cors,403오류 수정

--- todis-web/todis-web/src/main/java/com/todis/todisweb/demo/controller/CodyController.java
+++ todis-web/todis-web/src/main/java/com/todis/todisweb/demo/controller/CodyController.java
@@ -18,6 +18,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@CrossOrigin
 @RestController
 public class CodyController {
 
